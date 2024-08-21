a4e82c42495ab170d73fcfc988fb220184629d5b
TingtingLiao <tingtingliao000@gmail.com>
2024-08-21 04:40:30 +0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,15 @@ setup(
     version='0.0.1',
     description='video processing tools', 
     packages=find_packages(),
+    entry_points={
+        # CLI tools
+        'console_scripts': [
+            'vtl = main:main', 
+        ],
+    },
     install_requires=[ 
         'numpy',
         'tqdm', 
-        'cv2',  
+        'opencv-python',  
     ],
 )
