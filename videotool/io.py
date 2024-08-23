a75d3a07973abdf266d6d70df035eaafcf6a1c74
TingtingLiao <tingtingliao000@gmail.com>
2024-08-23 20:20:42 +0400

    io:add export video

--- videotool/io.py
+++ videotool/io.py
@@ -2,6 +2,7 @@ import os
 import os.path as osp 
 import cv2
 from PIL import Image
+from rich.progress import track
 
 import coloredlogs, logging 
 logger = logging.getLogger(__name__) 
@@ -33,7 +34,31 @@ def load_mp4(video_path):
   
         frames.append(frame)
 
-    return frames 
+    return frames
+
+
+def export_video(images, wfp, **kwargs):
+    fps = kwargs.get('fps', 30)
+    video_format = kwargs.get('format', 'mp4')  # default is mp4 format
+    codec = kwargs.get('codec', 'libx264')  # default is libx264 encoding
+    quality = kwargs.get('quality')  # video quality
+    pixelformat = kwargs.get('pixelformat', 'yuv420p')  # video pixel format
+    image_mode = kwargs.get('image_mode', 'rgb')
+    macro_block_size = kwargs.get('macro_block_size', 2)
+    ffmpeg_params = ['-crf', str(kwargs.get('crf', 18))]
+
+    writer = imageio.get_writer(
+        wfp, fps=fps, format=video_format,
+        codec=codec, quality=quality, ffmpeg_params=ffmpeg_params, pixelformat=pixelformat, macro_block_size=macro_block_size
+    )
+ 
+    for i in track(range(len(images)), description='Writing', transient=True):
+        if image_mode.lower() == 'bgr':
+            writer.append_data(images[i][..., ::-1])
+        else:
+            writer.append_data(images[i])
+
+    writer.close()
 
 
 
